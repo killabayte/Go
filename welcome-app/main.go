91741416715f259ee3ccfcd1e2ddf7cc14bbc1af
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-02-10 15:13:09 +0100

    Welcome-app updated with if conditions

--- welcome-app/main.go
+++ welcome-app/main.go
@@ -6,6 +6,13 @@ import (
 )
 
 func main() {
+
 	name := os.Args
+
+	if len(name) < 2 {
+		fmt.Println("Usege: ./main <name>")
+		os.Exit(1)
+	}
+
 	fmt.Printf("Hi %v! You are pretty mutch wellcome to that awesome Golang for DevOps cource!", name[1:])
 }
