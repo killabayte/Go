30b4b334639a5d30218c20b57b689c4dfc68326a
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-03-14 18:42:04 +0100

    Added new array and slice example

---
+++ array-and-slice/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr1 [7]int = [7]int{7, 3, 6, 0, 4, 9, 10}
+	fmt.Println(arr1)
+	fmt.Printf("%d %d\n", len(arr1), cap(arr1))
+	var arr2 []int = arr1[1:3]
+	fmt.Println(arr2)
+	fmt.Printf("%d %d\n", len(arr2), cap(arr2))
+}
