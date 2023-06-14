71303934e4affcd7400f1794795963157482c499
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-06-14 10:47:01 +0200

    Cover type-switch case

--- type-switch/main.go
+++ type-switch/main.go
@@ -1,9 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	//Blue-print: type switch part one
-	fmt.Println("type-switch representation")
-	//Blue-print: end comment seesion
+	var t1 string = "This is a string"
+	var t2 *string = &t1
+	discoverType(t2)
+	var t3 int = 42
+	discoverType(t3)
+	discoverType(nil)
+}
+
+func discoverType(t any) {
+	switch v := t.(type) {
+	case string:
+		t2 := v + "..."
+		fmt.Printf("String found: %s\n", t2)
+	case *string:
+		fmt.Printf("Pointer string found: %s\n", *v)
+	case int:
+		fmt.Printf("Int type found: %d\n", v)
+	default:
+		myType := reflect.TypeOf(t)
+		if myType == nil {
+			fmt.Printf("type is nil")
+		} else {
+			fmt.Printf("Type not found: %s\n", myType)
+		}
+	}
 }
