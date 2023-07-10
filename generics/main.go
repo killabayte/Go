2337a0b12c6085b59d37b9c14a6380f7a89fb51b
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-10 22:20:09 +0200

    Generic example

--- generics/main.go
+++ generics/main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var t1 int = 41
-	fmt.Printf("plusOne: %d\n", plusOne(t1))
+	var t1 int = 123
+	fmt.Printf("plusOne: %v (type: %s)\n", plusOne(t1), reflect.TypeOf(plusOne(t1)))
+	var t2 float64 = 123.12
+	fmt.Printf("plusOne: %v (type: %s)\n", plusOne(t2), reflect.TypeOf(plusOne(t2)))
 }
 
-func plusOne(t int) int {
+func plusOne[V int | float64 | int64 | float32 | int32](t V) V {
 	return t + 1
 }
