43b79fa423f368a3868d9da0f10d7fd0e9fc56e8
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-11 13:45:25 +0200

    Added json-parsing

---
+++ json-parsing/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"reflect"
+)
+
+type MyJson struct {
+	Test  any    `json:"test"`
+	Test3 string `json:"test3"`
+}
+
+func main() {
+	var JsonParsed MyJson
+	err := json.Unmarshal([]byte(`{"test": {"test2": [1,2,3]}, "test3": "..." }`), &JsonParsed)
+	if err != nil {
+		log.Fatal(err)
+	}
+	switch v := JsonParsed.Test.(type) {
+	case map[string]any:
+		fmt.Printf("Map found: %v\n", v)
+		field1, ok := v["test2"]
+		if ok {
+			switch v2 := field1.(type) {
+			case []any:
+				fmt.Printf("I found a []any\n")
+				for _, v2Element := range v2 {
+					fmt.Printf("Type: %s\n", reflect.TypeOf(v2Element))
+					if reflect.TypeOf(v2Element).String() == "float64" {
+						fmt.Printf("Int: %d\n", int(v2Element.(float64)))
+					} else {
+						fmt.Printf("Didn't recognize v2Element\n")
+					}
+				}
+			default:
+				fmt.Printf("Type not found: %s\n", reflect.TypeOf(v2))
+			}
+		}
+
+	default:
+		fmt.Printf("Type not found: %s\n", reflect.TypeOf(JsonParsed))
+	}
+
+}
