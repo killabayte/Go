c6f689b8a21a68c0184248027feb96cf486e7a19
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-20 17:07:01 +0200

    Go methods example, v0.6

--- methods/guess.go
+++ methods/guess.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
 	target := rand.Intn(100) + 1
+	fmt.Println("I've chosen a random number between 1 and 100.\nCan you guess it?")
 	fmt.Println(target)
 }
