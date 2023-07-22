4424d349d1f07f90f6d123e60108b34bc9c471fd
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-22 17:34:57 +0200

    Go functions example, v0.1

--- functions/paint.go
+++ functions/paint.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func paintNeeded(width float64, height float64) {
+func paintNeeded(width float64, height float64) float64 {
 	area := width * height
 	fmt.Printf("%.2f liters needed\n", area/10.0)
 }
