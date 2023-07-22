7678c13269513fc8d67e386e734a2edd2ea197bd
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-22 17:40:15 +0200

    Go functions example, v0.1

--- functions/paint.go
+++ functions/paint.go
@@ -4,11 +4,16 @@ import "fmt"
 
 func paintNeeded(width float64, height float64) float64 {
 	area := width * height
-	fmt.Printf("%.2f liters needed\n", area/10.0)
+	return area / 10.0
 }
 
 func main() {
-	paintNeeded(4.2, 3.0)
-	paintNeeded(5.2, 3.5)
-	paintNeeded(5.0, 3.3)
+	var amount, total float64
+	amount = paintNeeded(4.2, 3.0)
+	fmt.Printf("%0.2f liters needed\n", amount)
+	total += amount
+	amount = paintNeeded(5.2, 3.5)
+	fmt.Printf("%0.2f liters needed\n", amount)
+	total += amount
+	fmt.Printf("Total: %0.2f liters\n", total)
 }
