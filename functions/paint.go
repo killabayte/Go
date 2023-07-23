6a7741ac10fcb7af90c7a6c4f38a5386ba97283e
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-23 18:37:40 +0200

    Go functions example, v0.1

--- functions/paint.go
+++ functions/paint.go
@@ -6,16 +6,25 @@ func paintNeeded(width float64, height float64) (float64, error) {
 	if width < 0 {
 		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
 	}
+	if height < 0 {
+		return 0, fmt.Errorf("a height of %0.2f is invalid", height)
+	}
 	area := width * height
-	return area / 10.0
+	return area / 10.0, nil
 }
 
 func main() {
 	var amount, total float64
-	amount = paintNeeded(4.2, 3.0)
+	amount, err := paintNeeded(4.2, 3.0)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Printf("%0.2f liters needed\n", amount)
 	total += amount
-	amount = paintNeeded(5.2, 3.5)
+	amount, err = paintNeeded(5.2, 3.5)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Printf("%0.2f liters needed\n", amount)
 	total += amount
 	fmt.Printf("Total: %0.2f liters\n", total)
