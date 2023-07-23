a77e49c52bea026d76f41899d345e40224462042
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-23 18:34:18 +0200

    Go functions example, v0.1

--- functions/paint.go
+++ functions/paint.go
@@ -2,7 +2,10 @@ package main
 
 import "fmt"
 
-func paintNeeded(width float64, height float64) float64 {
+func paintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
 	area := width * height
 	return area / 10.0
 }
