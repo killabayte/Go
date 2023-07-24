3e177aea96c851e7f1e4ddf4f24031d8c9f7c34f
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-24 10:57:02 +0200

    Go functions example, v0.1

---
+++ functions/pointers.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var myInt int
+	// var myIntPointer *int
+	// myInt = 42
+	// myIntPointer = &myInt
+	// fmt.Println(*myIntPointer)
+	var myInt int
+	myInt = 42
+	myIntPointer := &myInt
+	fmt.Println(*myIntPointer)
+
+}
