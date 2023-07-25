f577b35b082cd492afec4211ffc4717092ab7699
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-25 16:59:13 +0200

    Go packages example, v0.1

--- packages/tocelsius.go
+++ packages/tocelsius.go
@@ -1,31 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"keyboard"
 	"log"
-	"os"
-	"strconv"
-	"strings"
 )
-
-func getFloat() (float64, error) {
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		return 0, err
-	}
-	input = strings.TrimSpace(input)
-	number, err := strconv.ParseFloat(input, 64)
-	if err != nil {
-		return 0, err
-	}
-	return number, nil
-}
 
 func main() {
 	fmt.Println("Enter a tempreture in Fahrenheit: ")
-	fahrenheit, err := getFloat()
+	fahrenheit, err := keyboard.GetFloat()
 	if err != nil {
 		log.Fatal(err)
 	}
