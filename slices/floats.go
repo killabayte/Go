0aaeb17c2bb21aebd80ff4d8c1ab3d26399c61a4
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-27 11:41:08 +0200

    Go slices example, v0.1

---
+++ slices/floats.go
@@ -0,0 +1,33 @@
+// This package read data from file
+package datafile
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+// GetFloats read meaning float64 from each string of file
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
+	file, err := os.Open(fileName)
+	if err != nil {
+		return numbers, err
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return numbers, err
+		}
+		numbers = append(numbers, number)
+	}
+	err = file.Close()
+	if err != nil {
+		return numbers, err
+	}
+	if scanner.Err() != nil {
+		return numbers, scanner.Err()
+	}
+	return numbers, nil
+}
