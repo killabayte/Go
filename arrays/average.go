311f87060376d64cc8b188e8652f58a51c06fd7b
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-27 10:52:42 +0200

    Go arrays example, v0.2

--- arrays/average.go
+++ arrays/average.go
@@ -1,14 +1,22 @@
 // average counting average value
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/headfirstgo/datafile"
+)
 
 func main() {
-	numbers := [3]float64{71.8, 56.2, 89.5}
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 	var sum float64 = 0
 	for _, number := range numbers {
 		sum += number
 	}
 	sampleCount := float64(len(numbers))
-	fmt.Printf("Average: %0.2f", sum/sampleCount)
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
 }
