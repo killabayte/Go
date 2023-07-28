de76c2d84aa589f42052a0574eb6508cee2fc1b5
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-28 12:28:30 +0200

    Go structs example, v0.3

--- struct/new_type.go
+++ struct/new_type.go
@@ -17,11 +17,11 @@ func main() {
 	kia.name = "rio"
 	kia.topSpeed = 220
 	fmt.Println("Name:", kia.name)
-	fmt.println("Top speed:", kia.topSpeed)
+	fmt.Println("Top speed:", kia.topSpeed)
 
 	var bolts parts
 	bolts.description = "hex bolts"
 	bolts.count = 42
 	fmt.Println("Description:", bolts.description)
-	fmt.println("Count:", bolts.count)
+	fmt.Println("Count:", bolts.count)
 }
