352fea0fefb5d5fc8d9b8a59d5535d9f85cd183f
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-30 17:26:43 +0200

    Go structs example, v0.4

--- struct/main.go
+++ struct/main.go
@@ -13,9 +13,9 @@ func main() {
 	subscriber.HomeAddress.State = "NE"
 	subscriber.HomeAddress.PostalCode = "68111"
 	fmt.Println("Subscriber name:", subscriber.Name)
-	fmt.Println("Subscriber street:", subscriber.HomeAddress.street)
-	fmt.Println("Subscriber city:", subscriber.HomeAddress.city)
-	fmt.Println("Subscriber state:", subscriber.HomeAddress.state)
+	fmt.Println("Subscriber street:", subscriber.HomeAddress.Street)
+	fmt.Println("Subscriber city:", subscriber.HomeAddress.City)
+	fmt.Println("Subscriber state:", subscriber.HomeAddress.State)
 	fmt.Println("Subscriber postal code:", subscriber.HomeAddress.PostalCode)
 
 	employee := magazine.Employee{Name: "Joy Carr"}
@@ -24,9 +24,9 @@ func main() {
 	employee.HomeAddress.State = "OR"
 	employee.HomeAddress.PostalCode = "97222"
 	fmt.Println("Employee name:", employee.Name)
-	fmt.Println("Employee street:", employee.HomeAddress.street)
-	fmt.Println("Employee city:", employee.HomeAddress.city)
-	fmt.Println("Employee state:", employee.HomeAddress.state)
+	fmt.Println("Employee street:", employee.HomeAddress.Street)
+	fmt.Println("Employee city:", employee.HomeAddress.City)
+	fmt.Println("Employee state:", employee.HomeAddress.State)
 	fmt.Println("Employee postal code:", employee.HomeAddress.PostalCode)
 
 }
