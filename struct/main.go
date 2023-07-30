4e5452c307405e1e996fe67d9637a67ffca0ab2d
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-30 16:51:56 +0200

    Go structs example, v0.4

--- struct/main.go
+++ struct/main.go
@@ -7,10 +7,9 @@ import (
 )
 
 func main() {
-	var address magazine.Address
-	address.Street = "1357 Odd st"
-	address.City = "Omaha"
-	address.State = "NE"
-	address.PostalCode = "68111"
-	fmt.Println(address)
+	address := magazine.Address{Street: "1357 Odd St", City: "Omaha", State: "NE", PostalCode: "68111"}
+	subscriber := magazine.Subscriber{Name: "Aman Singh"}
+	subscriber.HomeAddress = address
+	fmt.Println(subscriber.HomeAddress)
+
 }
