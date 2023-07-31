0fd78f328cabb0025193074b3a3a1f9e4a54ade0
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-31 15:53:57 +0200

    Go defined types example, v0.2

--- defined_type/main.go
+++ defined_type/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type Liters float64
 type Gallons float64
 type Milliliters float64
@@ -14,9 +12,14 @@ func (m Milliliters) ToGallons() Gallons {
 	return Gallons(m * 0.000264)
 }
 
+func (l Liters) ToMilliliters() Milliliters {
+	return Milliliters(l * 1000)
+}
+
+func (m Milliliters) ToLiters() Liters {
+	return Liters(m / 1000)
+}
+
 func main() {
-	soda := Liters(2)
-	fmt.Printf("%0.3f liters equals %0.3f gallons\n", soda, soda.ToGallons())
-	water := Milliliters(500)
-	fmt.Printf("%0.3f milliliters equals %0.3f gallons\n", water, water.ToGallons())
+
 }
