d1cfa726e82d37d1d2420a1f4ace587ed59dd10a
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-01 11:09:42 +0200

    Go encapsulation_and_embedding example, v0.3

--- encapsulation_and_embedding/main.go
+++ encapsulation_and_embedding/main.go
@@ -8,15 +8,20 @@ import (
 )
 
 func main() {
-	coordinates := geo.Coordinates{}
-	err := coordinates.SetLatitude(37.42)
+	location := geo.Landmark{}
+	err := location.SetName("The Googleplex")
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = coordinates.SetLongitude(-1122.08)
+	err = location.SetLongitude(-122.08)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(coordinates.Longitude)
-	fmt.Println(coordinates.Latitude)
+	err = location.SetLatitude(37.42)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(location.Name())
+	fmt.Println(location.Latitude())
+	fmt.Println(location.Longitude())
 }
