4175c43af7f7aadc1e77266b54a89d7b03174393
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-03 17:45:15 +0200

    Go recovering from failure example, v0.1

--- recovering-from-failure/recursion.go
+++ recovering-from-failure/recursion.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func count(start int, end int) {
+	fmt.Printf("count(%d, %d) called\n", start, end)
 	fmt.Println(start)
 	if start < end {
 		count(start+1, end)
 	}
+	fmt.Printf("Returning from count(%d, %d) call\n", start, end)
 }
 
 func main() {
