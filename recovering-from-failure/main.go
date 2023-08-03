7cfd09f891d4692a595c68a8f16c95f103c4ee4b
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-03 16:07:37 +0200

    Go recovering from failure example, v0.1

--- recovering-from-failure/main.go
+++ recovering-from-failure/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func OpenFile(fileName string) (*os.File, error) {
@@ -12,4 +14,25 @@ func OpenFile(fileName string) (*os.File, error) {
 func CloseFile(file *os.File) {
 	fmt.Println("Closing file")
 	file.Close()
+}
+
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
+	file, err := OpenFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return nil, err
+		}
+		numbers = append(numbers, number)
+	}
+	CloseFile(file)
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+	return numbers, nil
 }
