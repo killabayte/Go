385ba2bd0200eee3cc0269a8becfe5e2c581a21b
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-03 15:58:21 +0200

    Go recovering from failure example, v0.1

--- recovering-from-failure/main.go
+++ recovering-from-failure/main.go
@@ -9,3 +9,7 @@ func OpenFile(fileName string) (*os.File, error) {
 	fmt.Println("Opening", fileName)
 	return os.Open(fileName)
 }
+func CloseFile(file *os.File) {
+	fmt.Println("Closing file")
+	file.Close()
+}
