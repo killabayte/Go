f47bb98f04779cf80ff3d5788c8ec5b5312ed984
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-03 18:58:55 +0200

    Go recovering from failure example, v0.1

--- recovering-from-failure/snack.go
+++ recovering-from-failure/snack.go
@@ -3,16 +3,21 @@ package main
 import "fmt"
 
 func takoHunt() {
-	fmt.Println(recover())
+	p := recover()
+	err, ok := p.(error)
+	if ok {
+		fmt.Println(err.Error())
+	}
 }
 func snack() {
 	defer takoHunt()
 	fmt.Println("Opening refrigerator")
 	fmt.Println("Closing refrigerator")
-	panic("refrigerator is empty")
+	err := fmt.Errorf("There's an error")
+	panic(err)
 }
 
 func main() {
 	snack()
-	fmt.Println("Found a yesterday tako in a chair, no panic!")
+	fmt.Println("no panic! Found a yesterday tako in a chair!")
 }
