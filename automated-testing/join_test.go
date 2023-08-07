7b44fd26d8370e123a3263532e88867183ef8a75
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-07 22:22:23 +0200

    Go automated-testing, v0.1

--- automated-testing/join_test.go
+++ automated-testing/join_test.go
@@ -2,6 +2,9 @@ package prose
 
 import "testing"
 
+func TestTwoElements(t *testing.T) {
+	//t.Error("No test written yet"
+}
 func TestThreeElements(t *testing.T) {
-	t.Error("No test here either")
+	//t.Error("No test here either")
 }
