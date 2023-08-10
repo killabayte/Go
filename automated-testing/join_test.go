73a75a165a81725972d4a77357ebe6e3476d764f
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-10 07:49:25 +0200

    Go automated-testing, v0.1

--- automated-testing/join_test.go
+++ automated-testing/join_test.go
@@ -11,35 +11,16 @@ type testData struct {
 }
 
 // Run all: go test ~/go/src/github.com/headfirstgo/prose -v
-func TestOneElements(t *testing.T) {
-	list := []string{"apple"}
-	want := "apple"
-	got := JoinWithCommas(list)
-	if got != want {
-		t.Error(errorString(list, got, want))
+func TestJoinWithCommas(t *testing.T){
+	tests := []testData{
+		testData{list: []string{"apple"}, want: "apple"},
+		testData{list: []string{"apple", "orange"}, want: "apple and orange"},
+		testData{list: []string{"apple", "orange", "pear"}, want: "apple, orange, and pear"},
 	}
-}
-
-// Run only one: go test ~/go/src/github.com/headfirstgo/prose -v -run Two
-func TestTwoElements(t *testing.T) {
-	list := []string{"apple", "orange"}
-	want := "apple and orange"
-	got := JoinWithCommas(list)
-	if got != want {
-		t.Error(errorString(list, got, want))
+	for _, test := range tests {
+		got := JoinWithCommas(test.list)
+		if  got != test.want{
+			t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", test.list, got, test.want")
+		}
 	}
-}
-
-// Run all Elements: go test ~/go/src/github.com/headfirstgo/prose -v -run Elements
-func TestThreeElements(t *testing.T) {
-	list := []string{"apple", "orange", "pear"}
-	want := "apple, orange, and pear"
-	got := JoinWithCommas(list)
-	if got != want {
-		t.Error(errorString(list, got, want))
-	}
-}
-
-func errorString(list []string, got string, want string) string {
-	return fmt.Sprintf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
 }
