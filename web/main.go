6fa1fbbedfc9f84e67921ab94695fddce66159d8
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-11 09:40:21 +0200

    Go web, v0.1

---
+++ web/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	message := []byte("Hello, web!")
+	_, err := writer.Write(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
