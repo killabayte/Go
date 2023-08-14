5a7a0a146512d5c2916647326e0d1be0280e2597
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-14 10:40:40 +0200

    Go web, v0.1

--- web/first_class_function/pool.go
+++ web/first_class_function/pool.go
@@ -1,9 +1,17 @@
 package main
 
+import "fmt"
+
 func callFunction(passedFunction func()) {
 	passedFunction()
 }
 func callTwice(passedFunction func()) {
 	passedFunction()
 	passedFunction()
+}
+func callWithArguments(passedFunction func(string, bool)) {
+	passedFunction("This sentence is", false)
+}
+func printReturnValue(passedFunction func() string) {
+	fmt.Println(passedFunction())
 }
