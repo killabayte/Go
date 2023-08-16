6752ac3d942230ac917cb53aea56e6a9eec8ed9b
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-16 11:12:18 +0200

    Go html_templates, v0.1

--- html_templates/guest_book.go
+++ html_templates/guest_book.go
@@ -1,9 +1,18 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	placeholder := []byte("signature list goes here")
+	_, err := writer.Write(placeholder)
+	check(err)
 }
