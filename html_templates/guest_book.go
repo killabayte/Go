46ad126df65c8233c5946c4398dae4a3132348e6
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-17 18:39:50 +0200

    Go html_templates, v0.1

--- html_templates/guest_book.go
+++ html_templates/guest_book.go
@@ -13,7 +13,7 @@ func check(err error) {
 }
 
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
-	html, err := template.ParseFiles("html_templates/guest_book.html")
+	html, err := template.ParseFiles("view.html")
 	check(err)
 	err = html.Execute(writer, nil)
 	check(err)
