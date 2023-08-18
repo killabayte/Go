f29d308325cf3297e4f01eb320b316122be53d54
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-18 12:58:44 +0200

    Go html_templates, v0.1

--- html_templates/guest_book.go
+++ html_templates/guest_book.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
 }
 
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
