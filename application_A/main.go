364beb5f1cf8288ffd204be3b59015f18c2940da
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-20 17:45:12 +0200

    Go html_templates, v0.1

--- application_A/main.go
+++ application_A/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	file, err := os.Openfile("aardvark.txt", os.O_RDONLY, os.FileMode(0600))
+	file, err := os.OpenFile("aardvark.txt", os.O_RDONLY, os.FileMode(0600))
 	check(err)
 	defer file.Close()
-	scanner := bufio.NewScaner(file)
+	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 	}
