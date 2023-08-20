72bfc49ecdab58a6bed01c8adbf2c160c85a70d1
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-20 11:29:14 +0200

    Go html_templates, v0.1

--- html_templates/guest_book.go
+++ html_templates/guest_book.go
@@ -54,9 +54,16 @@ func newHandler(writer http.ResponseWriter, request *http.Request) {
 	check(err)
 }
 
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("signature")
+	_, err := writer.Write([]byte(signature))
+	check(err)
+}
+
 func main() {
 	http.HandleFunc("/guestbook", viewHandler)
 	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
