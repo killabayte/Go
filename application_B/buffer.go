5390e30588ff91b73f93b568cf16c8212d694167
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-22 11:04:47 +0200

    Go application_B, v0.1

--- application_B/buffer.go
+++ application_B/buffer.go
@@ -18,7 +18,7 @@ func sendLetters(channel chan string) {
 
 func main() {
 	fmt.Println(time.Now())
-	channel := make(chan string, 1)
+	channel := make(chan string, 3)
 	go sendLetters(channel)
 	time.Sleep(5 * time.Second)
 	fmt.Println(<-channel, time.Now())
