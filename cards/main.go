fcdb996eb86df27becf6de2371b3b8d8c02929cc
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-25 10:30:32 +0200

    Cards golang v0.0.1

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+	greeting := "Hi there!"
+	fmt.Println([]byte(greeting))
+
 }
