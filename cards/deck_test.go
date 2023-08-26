eb519a1f6098a7c474ba34e1a6b62fb957724662
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-26 16:37:06 +0200

    Cards golang v0.0.1

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -6,6 +6,6 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Expected deck length of 20, but got %v", len(d))
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
 }
