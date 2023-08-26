6771a902c528c29c9558879ed3929630d8b3d897
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-26 10:28:20 +0200

    Cards golang v0.0.1

--- cards/deck.go
+++ cards/deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -50,4 +51,15 @@ func newDeckFromFile(filename string) deck {
 
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource()
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
