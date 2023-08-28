c9538acd504d3caec682b49f7eca17d83d0f11a9
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-28 09:50:26 +0200

    Maps golang v0.0.1

--- maps_v2/main.go
+++ maps_v2/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#fF0000",
-		"green": "#00FF00",
-	}
+	// colors := map[string]string{
+	// 	"red":   "#fF0000",
+	// 	"green": "#00FF00",
+	// }
 	fmt.Println(colors)
 }
