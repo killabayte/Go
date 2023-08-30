bc88d740af8e3db5c9181cf744d361aadc272e01
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-30 12:32:08 +0200

    Interfaces golang v0.0.1

--- interfaces_v2/http/main.go
+++ interfaces_v2/http/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
 	resp, err := http.Get("https://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(resp)
 }
