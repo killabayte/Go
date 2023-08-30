a05348223e438fd3fb8ee695aaf8b39c4e5141e8
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-08-30 09:41:14 +0200

    Interfaces golang v0.0.1

--- interfaces_v2/main.go
+++ interfaces_v2/main.go
@@ -3,6 +3,14 @@ package main
 type englishBot struct {
 }
 
+type spanishBot struct {
+}
+
 func main() {
+
+}
+
+func (eb englishBot) getGreeting() string {
+	// Very custom logic for generation an greeting
 
 }
