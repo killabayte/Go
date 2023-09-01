c474cef3b77c5b53a3d38ba8d7dd258d90f08d8f
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-01 12:40:49 +0200

    Channels golang v0.0.1

--- channels/main.go
+++ channels/main.go
@@ -1,5 +1,11 @@
 package main
 
 func main() {
-
+	links := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://stackoverflow.com",
+		"https://golang.com",
+		"https://amazon.com",
+	}
 }
