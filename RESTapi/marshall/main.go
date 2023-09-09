2ba1ec88f489fdbc9ff958951152d14aca08f103
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-09 16:19:23 +0200

    Golnag RESTapi

--- RESTapi/marshall/main.go
+++ RESTapi/marshall/main.go
@@ -1,6 +1,13 @@
 package main
 
 type Professor struct {
-	Name      string `json:"name"`
-	ScienseID int    `json:science_id`
+	Name       string     `json:"name"`
+	ScienseID  int        `json:science_id`
+	IsWorking  bool       `json:is_working`
+	University University `json:university`
+}
+
+type University struct {
+	Name string `json:"name"`
+	Cuty string `json:city`
 }
