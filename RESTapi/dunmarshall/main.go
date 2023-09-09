9f13b2a0c7082cb0a9661d4b5f48248ed6ef7625
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-09 16:07:24 +0200

    Golnag RESTapi

---
+++ RESTapi/dunmarshall/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	jsonFile, err := os.Open("users.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer jsonFile.Close()
+	fmt.Println("File descriptor sucessfully created")
+
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var result map[string]interface{}
+	json.Unmarshal(byteValue, &result)
+}
