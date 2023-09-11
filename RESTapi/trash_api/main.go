e591fb472e4c723f504e47702477cdfeda9f2ac3
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-11 11:10:01 +0200

    Golnag RESTapi

--- RESTapi/trash_api/main.go
+++ RESTapi/trash_api/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 var (
 	port string = "8080"
 )
+
+func main() {
+	log.Println("Trying to start REST API pizza!")
+	log.Fatal(http.ListenAndServe(":8080"+port, nil))
+}
