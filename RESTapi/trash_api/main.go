73cbc0f49e5856ca09932265cac32b8a95e4c745
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-11 15:05:28 +0200

    Golnag RESTapi

--- RESTapi/trash_api/main.go
+++ RESTapi/trash_api/main.go
@@ -66,7 +66,7 @@ func GetAllPizzas(writer http.ResponseWriter, request *http.Request) {
 func GetPizzaById(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(request) // { "id": "42" }
-	id, err := strconv.Atoi()
+	id, err := strconv.Atoi(vars["id"])
 }
 
 func main() {
