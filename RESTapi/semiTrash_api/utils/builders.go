7802b0f5325d4fef320165985321096c2298444e
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-15 18:27:16 +0200

    Golnag RESTapi

--- RESTapi/semiTrash_api/utils/builders.go
+++ RESTapi/semiTrash_api/utils/builders.go
@@ -5,7 +5,10 @@ import (
 )
 
 func BuildBookResource(router *mux.Router, prefix string) {
-
+	router.HandleFunc(prefix+"/{id}", GetBookByID).Methods("GET")
+	router.HandleFunc(prefix, CreateBook).Methods("POST")
+	router.HandleFunc(prefix+"/{id}", UpdateBookByID).Methods("PUT")
+	router.HandleFunc(prefix+"/{id}", DeleteBookByID).Methods("DELETE")
 }
 
 func BuildBooksResource(router *mux.Router, prefix string) {
