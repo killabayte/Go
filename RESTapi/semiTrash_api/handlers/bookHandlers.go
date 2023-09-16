2f4714bbfb57118cc831222ddb30825a55e413c7
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-16 17:02:53 +0200

    Golnag RESTapi

--- RESTapi/semiTrash_api/handlers/bookHandlers.go
+++ RESTapi/semiTrash_api/handlers/bookHandlers.go
@@ -1,6 +1,8 @@
 package handlers
 
-func GetBookByID()    {}
-func CreateBook()     {}
-func UpdateBookByID() {}
-func DeleteBookByID() {}
+import "net/http"
+
+func GetBookByID(writer http.ResponseWriter, request *http.Request)    {}
+func CreateBook(writer http.ResponseWriter, request *http.Request)     {}
+func UpdateBookByID(writer http.ResponseWriter, request *http.Request) {}
+func DeleteBookByID(writer http.ResponseWriter, request *http.Request) {}
