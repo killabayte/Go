5011184074df2d46e86b561038782c247064d201
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-16 17:03:07 +0200

    Golnag RESTapi

--- RESTapi/semiTrash_api/utils/builders.go
+++ RESTapi/semiTrash_api/utils/builders.go
@@ -1,16 +1,16 @@
 package utils
 
 import (
-	handlers "semiTrash_api/handlers"
+	"semiTrash_api/handlers"
 
 	"github.com/gorilla/mux"
 )
 
 func BuildBookResource(router *mux.Router, prefix string) {
-	router.HandleFunc(prefix+"/{id}", GetBookByID).Methods("GET")
-	router.HandleFunc(prefix, CreateBook).Methods("POST")
-	router.HandleFunc(prefix+"/{id}", UpdateBookByID).Methods("PUT")
-	router.HandleFunc(prefix+"/{id}", DeleteBookByID).Methods("DELETE")
+	router.HandleFunc(prefix+"/{id}", handlers.GetBookByID).Methods("GET")
+	router.HandleFunc(prefix, handlers.CreateBook).Methods("POST")
+	router.HandleFunc(prefix+"/{id}", handlers.UpdateBookByID).Methods("PUT")
+	router.HandleFunc(prefix+"/{id}", handlers.DeleteBookByID).Methods("DELETE")
 }
 
 func BuildBooksResource(router *mux.Router, prefix string) {
