4c26e33b7d0a09cd2c7b5f3c3391858b4a5b5d86
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-17 15:58:24 +0200

    Golnag RESTapi

--- RESTapi/semiTrash_api/handlers/bookHandlers.go
+++ RESTapi/semiTrash_api/handlers/bookHandlers.go
@@ -1,9 +1,15 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
 
 func GetBookByID(writer http.ResponseWriter, request *http.Request) {
 	initHeaders(writer)
+	id, err := strconv.Atoi(mux.Vars(request)["id"])
 }
 func CreateBook(writer http.ResponseWriter, request *http.Request) {
 	initHeaders(writer)
