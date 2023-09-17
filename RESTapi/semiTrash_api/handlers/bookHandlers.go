85398c1f00a33e1b5a29e084ec8538c1c8be7d45
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-17 16:01:18 +0200

    Golnag RESTapi

--- RESTapi/semiTrash_api/handlers/bookHandlers.go
+++ RESTapi/semiTrash_api/handlers/bookHandlers.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
+
+	models "command-line-arguments/Users/ykostiannikov/workspace/Go/RESTapi/semiTrash_api/models/helper.go"
 
 	"github.com/gorilla/mux"
 )
@@ -10,6 +13,13 @@ import (
 func GetBookByID(writer http.ResponseWriter, request *http.Request) {
 	initHeaders(writer)
 	id, err := strconv.Atoi(mux.Vars(request)["id"])
+	if err := nil{
+		log.Println("error while parsing happend:", err)
+		writer.WriteHeader(400)
+		msg := models.Message{Message: "do not use parameter ID as uncasted to int type"}
+		json.NewEncoder(writer).Encode(msg)
+	}
+	book, ok := models.FindBookById(id)
 }
 func CreateBook(writer http.ResponseWriter, request *http.Request) {
 	initHeaders(writer)
