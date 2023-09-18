c356c1a8e6da56ec9f41ed5c5e6dcc5eca7ac1d9
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-18 16:06:31 +0200

    Golnag RESTapi

--- RESTapi/StandardWebServer/cmd/api/main.go
+++ RESTapi/StandardWebServer/cmd/api/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"log"
 
+	"github.com/BurntSushi/toml"
 	"github.com/killabayte/Go/RESTapi/StandardWebServer/internal/app/api"
 )
 
-var ()
+var (
+	configPath string = "configs/api.toml"
+)
 
 func init() {
 }
@@ -14,6 +17,8 @@ func init() {
 func main() {
 	log.Println("It works!")
 	config := api.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+
 	server := api.New(config)
 
 	if err := server.Start(); err != nil {
