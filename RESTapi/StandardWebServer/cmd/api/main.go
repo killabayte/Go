5189fac3440238f1f8c773a7bfd35742c9e431bb
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-18 18:48:10 +0200

    Golnag RESTapi

--- RESTapi/StandardWebServer/cmd/api/main.go
+++ RESTapi/StandardWebServer/cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/BurntSushi/toml"
@@ -13,16 +14,20 @@ var (
 )
 
 func init() {
-	flag.StringVar(&configPath, "config-path", "configs/api.toml", "path to config file in toml format")
+	flag.StringVar(&configPath, "path", "configs/api.toml", "path to config file")
 }
 
 func main() {
 	flag.Parse()
+	if configPath == "" {
+		fmt.Println("No config path specified, using default")
+		configPath = "configs/api.toml"
+	}
 	log.Println("It works!")
 	config := api.NewConfig()
 	_, err := toml.DecodeFile(configPath, config)
 	if err != nil {
-		log.Println("Can't decode config file", err)
+		log.Println("Error decoding config file:", err)
 	}
 
 	server := api.New(config)
--- RESTapi/StandardWebServer/go.mod
+++ RESTapi/StandardWebServer/go.mod
@@ -2,4 +2,7 @@ module github.com/killabayte/Go/RESTapi/StandardWebServer
 
 go 1.21.0
 
-require github.com/BurntSushi/toml v1.3.2 // indirect
+require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
