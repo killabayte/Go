fad6d14ec6c1e0c162d8f4a41090d1360690334e
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-19 13:47:24 +0200

    Golnag RESTapi

--- RESTapi/StandardWebServer/cmd/api/main.go
+++ RESTapi/StandardWebServer/cmd/api/main.go
@@ -4,8 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/BurntSushi/toml"
+	"github.com/joho/godotenv"
 	"github.com/killabayte/Go/RESTapi/StandardWebServer/internal/app/api"
 )
 
@@ -21,19 +23,38 @@ func init() {
 
 func main() {
 	flag.Parse()
-	log.Println("It works!")
-
 	config := api.NewConfig()
 
 	if configPath == "" || format == "" {
-		fmt.Println("No config path specified, using default")
-		configPath = "configs/api.toml"
+		fmt.Println("No config path specified, using local .toml file as default")
+		_, err := os.Stat("configs/api.toml")
+		if err == nil {
+			configPath = "configs/api.toml"
+			_, err := toml.DecodeFile(configPath, config)
+			if err != nil {
+				log.Println("Error decoding config file:", err)
+			}
+		} else {
+			fmt.Println("No config file provided and no config file found locally, using default values from the code")
+		}
+	} else if format == ".toml" {
+		fmt.Println("Got an .toml format config file")
 		_, err := toml.DecodeFile(configPath, config)
 		if err != nil {
-			log.Println("Error decoding config file:", err)
+			log.Println("Error decoding provided .toml config file:", err)
 		}
-		fmt.Println(config)
+	} else if format == ".env" {
+		fmt.Println("Got an .env format config file")
+		err := godotenv.Load(configPath)
+		if err != nil {
+			log.Println("Error decoding provided .env config file:", err)
+		}
+		bind_addr := os.Getenv("bind_addr")
+		config.BindAddr = bind_addr
 	}
+
+	log.Println("It works!")
+	fmt.Println(config)
 	server := api.New(config)
 
 	if err := server.Start(); err != nil {
