7110a4fe8e097d7394a6496c7cb7a09be82cd41d
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-09-21 17:22:38 +0200

    Golnag RESTapi

---
+++ RESTapi/StandardWebServer/internal/app/api/helper.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+func (a *API) configureLoggerField() error {
+	log_level := logrus.ParseLevel(a.config.LoggerLevel)
+	if err != nil {
+		return err
+	}
+	a.logger.SetLevel(log_level)
+	return nil
+}
