aaf57da4fe7a2ddf327caed0f5221bea141f434f
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-11-03 17:21:37 +0100

    API: DB and schemas V2

--- RESTapi/ServerAndDBV2/storage/userrepository.go
+++ RESTapi/ServerAndDBV2/storage/userrepository.go
@@ -1,5 +1,23 @@
 package storage
 
+import "github.com/killabayte/Go/RESTapi/ServerAndDBV2/internal/app/models"
+
 type UserRepository struct {
 	storage *Storage
+}
+
+var (
+	tableUser string = "users"
+)
+
+func (ur *UserRepository) Create(u *models.User) (*models.User, error) {
+	return nil, nil
+}
+
+func (ur *UserRepository) FindByLogin(login string) (*models.User, bool, error) {
+	return nil, false, nil
+}
+
+func (ur *UserRepository) SelectAll() ([]*models.User, error) {
+	return nil, nil
 }
