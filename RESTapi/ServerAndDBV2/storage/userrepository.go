8a01aad88b1a9c83c94ec4f16cb217888a4d41cd
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-11-03 17:42:05 +0100

    API: DB and schemas V2

--- RESTapi/ServerAndDBV2/storage/userrepository.go
+++ RESTapi/ServerAndDBV2/storage/userrepository.go
@@ -1,6 +1,11 @@
 package storage
 
-import "github.com/killabayte/Go/RESTapi/ServerAndDBV2/internal/app/models"
+import (
+	"fmt"
+	"log"
+
+	"github.com/killabayte/Go/RESTapi/ServerAndDBV2/internal/app/models"
+)
 
 type UserRepository struct {
 	storage *Storage
@@ -11,7 +16,11 @@ var (
 )
 
 func (ur *UserRepository) Create(u *models.User) (*models.User, error) {
-	return nil, nil
+	query := fmt.Sprintf("INSERT INTO %s (login, password) VALUES ($1, $2) RETURNING id", tableUser)
+	if err := ur.storage.db.QueryRow(query, u.Login, u.Password).Scan(&u.ID); err != nil {
+		return nil, err
+	}
+	return u, nil
 }
 
 func (ur *UserRepository) FindByLogin(login string) (*models.User, bool, error) {
@@ -19,5 +28,21 @@ func (ur *UserRepository) FindByLogin(login string) (*models.User, bool, error) 
 }
 
 func (ur *UserRepository) SelectAll() ([]*models.User, error) {
+	query := fmt.Sprintf("SELECT * FROM %s", tableUser)
+	rows, err := ur.storage.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	users := make([]*models.User, 0)
+	for rows.Next() {
+		u := models.User{}
+		err := rows.Scan(&u.ID, &u.Login, &u.Password)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		users = append(users, &u)
+	}
 	return nil, nil
 }
