582fdc90598cbdc85adba0b901ebd6c3553c1a9b
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-11-03 18:07:05 +0100

    API: DB and schemas V2

--- RESTapi/ServerAndDBV2/storage/articlerepository.go
+++ RESTapi/ServerAndDBV2/storage/articlerepository.go
@@ -1,5 +1,11 @@
 package storage
 
+import (
+	"fmt"
+
+	"github.com/killabayte/Go/RESTapi/ServerAndDBV2/internal/app/models"
+)
+
 type ArticleRepository struct {
 	storage *Storage
 }
@@ -7,3 +13,11 @@ type ArticleRepository struct {
 var (
 	tableArticle string = "articles"
 )
+
+func (ar *ArticleRepository) Create(a *models.Article) (*models.Article, error) {
+	query := fmt.Sprintf("INSERT INTO %s (title, author, context) VALUES ($1, $2, $3) RETURNING id", tableArticle)
+	if err := ar.storage.db.QueryRow(query, a.Title, a.Author, a.Context).Scan(&a.ID); err != nil {
+		return nil, err
+	}
+	return a, nil
+}
