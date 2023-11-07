37d7b8dc36ca75b1ed46dcfdc52197e8f2c89152
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-11-07 18:10:20 +0100

    API: DB and schemas V2

--- RESTapi/ServerAndDBV2/internal/app/api/handlers.go
+++ RESTapi/ServerAndDBV2/internal/app/api/handlers.go
@@ -1,1 +1,12 @@
 package api
+
+import "net/http"
+
+type Message struct {
+	StatusCode int    `json:"status_code"`
+	Message	string `json:"message"`
+	IsError bool `json:"is_error"`
+
+func initHeaders(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+}
