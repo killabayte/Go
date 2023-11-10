49f79769ba2286d141be8900c1f80318e668c30f
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-11-10 13:04:58 +0100

    API: DB and schemas V2

--- RESTapi/ServerAndDBV2/internal/app/api/handlers.go
+++ RESTapi/ServerAndDBV2/internal/app/api/handlers.go
@@ -88,11 +88,11 @@ func (api *API) GetArticleById(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		api.logger.Info("Error while Articles.FindArticleById(): ", err)
 		msg := Message{
-			StatusCode: 501,
+			StatusCode: 500,
 			Message:    "We have some troubles to access the database. Try again later.",
 			IsError:    true,
 		}
-		w.WriteHeader(501)
+		w.WriteHeader(500)
 		json.NewEncoder(w).Encode(msg)
 		return
 	}
