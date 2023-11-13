f674854bc164fdf6d758ed407e024b872df484c9
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-11-13 11:11:25 +0100

    API: DB and schemas V2

--- RESTapi/ServerAndDBV2/internal/app/api/handlers.go
+++ RESTapi/ServerAndDBV2/internal/app/api/handlers.go
@@ -138,6 +138,17 @@ func (api *API) DeleteArticleById(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(msg)
 		return
 	}
+	if !ok {
+		api.logger.Info("Article not found")
+		msg := Message{
+			StatusCode: 404,
+			Message:    "Article not found in database.",
+			IsError:    true,
+		}
+		w.WriteHeader(404)
+		json.NewEncoder(w).Encode(msg)
+		return
+	}
 
 }
 
