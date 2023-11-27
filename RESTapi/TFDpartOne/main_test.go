f3c05d7ace97ee908ff0b2fd83b4600a26852cc8
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-11-27 18:33:26 +0100

    API: Testing

--- RESTapi/TFDpartOne/main_test.go
+++ RESTapi/TFDpartOne/main_test.go
@@ -11,7 +11,9 @@ type TestCase struct {
 var cases []TestCase = []TestCase{
 	{Input: 0, Expected: 1},
 	{Input: 1, Expected: 1},
-	{Input: 3, Expected: 6}, 
+	{Input: 3, Expected: 6},
+	{Input: 5, Expected: 120},
+}
 
 func TestFactorial(t *testing.T) {
 
