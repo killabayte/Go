f0c954190e055f7f83a1d582745545a8318020eb
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-15 11:25:02 +0100

    ticketBooking: init

--- ticketBooking/main.go
+++ ticketBooking/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go for DevOps Conference"
+	conferenceName := "Go for DevOps Conference"
 	const conferenceTickets = 50
-	var remainingTickets = conferenceTickets
+	remainingTickets := conferenceTickets
 
 	fmt.Printf("Wellcome to our %s conference booking application\n", conferenceName)
 	fmt.Printf("We have total %d tickets for the conference, and %d still available.\n", conferenceTickets, remainingTickets)
