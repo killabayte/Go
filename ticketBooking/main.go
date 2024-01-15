10cd0cc2846129f65592fcf4bae8c2128a240f87
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-15 11:13:34 +0100

    ticketBooking: init

--- ticketBooking/main.go
+++ ticketBooking/main.go
@@ -7,8 +7,10 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = conferenceTickets
 
-	fmt.Printf("Wellcome to our %s conference booking application", conferenceName)
-	fmt.Printf("We have total %d tickets for the conference, and %d still available.", conferenceTickets, remainingTickets)
+	fmt.Printf("Wellcome to our %s conference booking application\n", conferenceName)
+	fmt.Printf("We have total %d tickets for the conference, and %d still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
 
 }
