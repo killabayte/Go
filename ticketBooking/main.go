13b804e038cc0a3eee78e17f3e4d1e0bb5eca442
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-15 10:54:47 +0100

    ticketBooking: init

--- ticketBooking/main.go
+++ ticketBooking/main.go
@@ -5,4 +5,6 @@ import "fmt"
 func main() {
 	fmt.Println("Wellcome to our DevCon conference booking application")
 	fmt.Println("Get your tickets here to attend")
+
+	var conferenceName = "Go for DevOps Conference"
 }
