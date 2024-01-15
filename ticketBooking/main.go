1532dd93dadec4f8ad3df64cece8b6cbf52571bd
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-15 18:15:38 +0100

    ticketBooking: init

--- ticketBooking/main.go
+++ ticketBooking/main.go
@@ -31,10 +31,10 @@ func main() {
 		firstNames := []string{}
 		for _, booking := range bookings {
 			var names = strings.Fields(booking)
-			var firstName = names[0]
-			firstNames = append(firstNames, firstName)
+			firstNames = append(firstNames, names[0])
 		}
 		fmt.Printf("All bookings what we have so far: %v\n", bookings)
+		fmt.Printf("All first names what we have so far: %v\n", firstNames)
 	}
 
 }
