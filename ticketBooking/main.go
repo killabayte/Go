6f19554ce7b547b5e354c26c12c50576c0d57966
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-16 22:48:26 +0100

    ticketBooking: init

--- ticketBooking/main.go
+++ ticketBooking/main.go
@@ -20,7 +20,7 @@ func main() {
 	for remainingTickets > 0 {
 
 		firstName, lastName, email, userTickets := getUserInput()
-		isNameValid, isEmailValid, isTicketsValid := ValidateUserInput(firstName, lastName, email, userTickets)
+		isNameValid, isEmailValid, isTicketsValid := validateUserInput(firstName, lastName, email, userTickets)
 
 		if isNameValid && isEmailValid && isTicketsValid {
 			bookTicket(userTickets, firstName, lastName, email)
