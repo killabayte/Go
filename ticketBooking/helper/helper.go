9229507599770aee3f5a3330e852d15cb078b987
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-16 22:58:44 +0100

    ticketBooking: init

--- ticketBooking/helper/helper.go
+++ ticketBooking/helper/helper.go
@@ -2,9 +2,9 @@ package helper
 
 import "strings"
 
-func ValidateUserInput(fn string, ln string, e string, ut uint) (bool, bool, bool) {
+func ValidateUserInput(fn string, ln string, e string, ut uint, rt uint) (bool, bool, bool) {
 	var isNameValid = len(fn) >= 2 && len(ln) > 2
 	var isEmailValid = strings.Contains(e, "@")
-	var isTicketsValid = ut > 0 && ut <= remainingTickets
+	var isTicketsValid = ut > 0 && ut <= rt
 	return isNameValid, isEmailValid, isTicketsValid
 }
