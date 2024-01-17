7f86d5cd6b875c67546778b70bafed45d8a5f121
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-17 10:19:23 +0100

    ticketBooking: init

--- ticketBooking/main.go
+++ ticketBooking/main.go
@@ -82,6 +82,6 @@ func bookTicket(userTickets uint, firstName string, lastName string, email strin
 func validateUserInput(fn string, ln string, e string, ut uint) (bool, bool, bool) {
 	var isNameValid = len(fn) >= 2 && len(ln) > 2
 	var isEmailValid = strings.Contains(e, "@")
-	var isTicketsValid = ut > 0 && ut <= rt
+	var isTicketsValid = ut > 0 && ut <= remainingTickets
 	return isNameValid, isEmailValid, isTicketsValid
 }
