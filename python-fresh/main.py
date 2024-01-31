83d55aa6808a551e288451a7643764fe30f95bd3
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-31 10:58:54 +0100

    Python refreshing memories

--- python-fresh/main.py
+++ python-fresh/main.py
@@ -2,3 +2,6 @@ calculation_to_units = 24
 name_of_unit = "hours"
 
 print(f"20 days are {20 * calculation_to_units} {name_of_unit}")
+
+def days_to_units(num_of_days):
+    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
