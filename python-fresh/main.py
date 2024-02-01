76d24ab27f8571f72536468f3054dff6654ed4c9
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-02-01 12:48:13 +0100

    Python refresh

--- python-fresh/main.py
+++ python-fresh/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 calculation_to_units = 24
 name_of_unit = "hours"
 num_of_days = 365
@@ -8,3 +9,5 @@ def days_to_units(num_of_days):
     return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
 
 print(days_to_units(num_of_days))
+
+# Path: python-fresh/main.py
