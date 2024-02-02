34c689610a25f2260d7378341eef87f5afac77e6
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-02-02 21:29:07 +0100

    Python refreshing memories

--- python-fresh/time-till-deadline.py
+++ python-fresh/time-till-deadline.py
@@ -1,4 +1,7 @@
 import datetime
 
 user_input = input ("Enter your goal with a deadline separated by a colon\n")
-user_input.split(":")
+input_list = user_input.split(":")
+
+goal = input_list[0]
+deadline = input_list[1]
