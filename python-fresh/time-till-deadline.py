f950f7de22ec676dccb21049cc6d6891979af4c2
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-02-03 10:58:28 +0100

    Python refreshing memories

--- python-fresh/time-till-deadline.py
+++ python-fresh/time-till-deadline.py
@@ -7,5 +7,6 @@ goal = input_list[0]
 deadline = input_list[1]
 
 print(datetime.datetime.strptime(deadline, '%d.%m.%Y'))
+print(type(datetime.datetime.strptime(deadline, '%d.%m.%Y')))
 
 print(input_list)
