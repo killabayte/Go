ed5bb2251ce78acf06ada5901b8b8d54de838d4c
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-07 17:46:20 +0200

    Back to basics

--- GoProgramming-TheCompleteDeveloperGuide/functions/functions.go
+++ GoProgramming-TheCompleteDeveloperGuide/functions/functions.go
@@ -14,8 +14,34 @@ func greet(name string) {
 	fmt.Println("Namaskar", name)
 }
 
+func namaste() string {
+	return "Namaste!"
+}
+
+func trikonasana(a, b, c int) int {
+	return a + b + c
+}
+
+func numReturn() int {
+	return 42
+}
+
+func multiNumReturn() (int, int) {
+	return 54, 54
+}
+
+func addThreeNum(a, b, c int) int {
+	return a + b + c
+}
+
 func main() {
-	greet("Matsyendranatha")
+	greet("Matsyendranatha!")
+	fmt.Println(namaste())
+	trikonasanaResult := trikonasana(1, 100, 7)
+	fmt.Println("Trikonasana result is:", trikonasanaResult)
+	b, c := multiNumReturn()
+	sumOfThree := addThreeNum(numReturn(), b, c)
+	fmt.Println("This is sum of three num:", sumOfThree)
 	dozen := double(6)
 	fmt.Println("This is dozen:", dozen)
 	bakerDozen := add(dozen, 1)
