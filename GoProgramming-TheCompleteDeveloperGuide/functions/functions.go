2baed90c5e9b63d42b5f633fec85c3bbec353b38
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-07 17:25:28 +0200

    Back to basics

---
+++ GoProgramming-TheCompleteDeveloperGuide/functions/functions.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func double(x int) int {
+	return x + x
+}
+
+func add(l, r int) int {
+	return l + r
+}
+
+func greet() {
+	fmt.Println("Hi, this is great day!")
+}
+
+func main() {
+	greet()
+	dozen := double(6)
+	fmt.Println("This is dozen:", dozen)
+	bakerDozen := add(dozen, 1)
+	fmt.Println("A baker dozen is:", bakerDozen)
+}
