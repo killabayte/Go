906a719f360ba9941925c4465cb69efada8d38b9
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-09 13:34:01 +0200

    Back to basics

--- GoProgramming-TheCompleteDeveloperGuide/switch/switch.go
+++ GoProgramming-TheCompleteDeveloperGuide/switch/switch.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	switch age := 12; {
+	switch age := 99; {
 	case age == 0:
 		fmt.Println("Newborn")
-	case age > 0 && age <= 3:
+	case age >= 1 && age <= 3:
 		fmt.Println("Toddler")
-	case age > 3 && age <= 12:
-		fmt.Println("child")
-	case age > 12 && age <= 17:
-		fmt.Println("teenager")
-	case age >= 18:
-		fmt.Println("adult")
+	case age < 13:
+		fmt.Println("Child")
+	case age < 18:
+		fmt.Println("Teenager")
+	default:
+		fmt.Println("Adult")
 	}
 }
