e9d712c39952dbd445174ca5ffbd099a917efae5
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-10 19:02:31 +0200

    Back to basics

--- GoProgramming-TheCompleteDeveloperGuide/structures/structures.go
+++ GoProgramming-TheCompleteDeveloperGuide/structures/structures.go
@@ -8,3 +8,11 @@ type Rectangle struct {
 	a Coordinate
 	b Coordinate
 }
+
+func width(rect Rectangle) int {
+	return (rect.b.x - rect.a.x)
+}
+
+func lenght(rect Rectangle) int {
+	return (rect.a.y - rect.b.y)
+}
