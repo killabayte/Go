12a9a0cdf5a083a68c23125ee4a1d808d7b24ddc
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-12 18:25:18 +0200

    Back to basics

--- GoProgramming-TheCompleteDeveloperGuide/arrays/arrays.go
+++ GoProgramming-TheCompleteDeveloperGuide/arrays/arrays.go
@@ -1,6 +1,27 @@
 package main
 
+import "fmt"
+
 type shoppingList struct {
-	price float64
 	name  string
+	price float64
+}
+
+func main() {
+	shoppingListActual := [4]shoppingList{
+		{name: "apple", price: 15.0},
+		{name: "milk", price: 2.5},
+		{name: "bekon", price: 22.5},
+	}
+
+	lastElement := shoppingListActual[(len(shoppingListActual) - 1)]
+	fmt.Println(lastElement)
+	totalListNum := len(shoppingListActual)
+	fmt.Println(totalListNum)
+
+	var totalCost float64
+	for i := 0; i < len(shoppingListActual); i++ {
+		totalCost += shoppingListActual[i].price
+	}
+	fmt.Println(totalCost)
 }
