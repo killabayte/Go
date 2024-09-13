3ff817f74ec126a00b9336221652b9cc4ef3d1dd
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-13 19:01:07 +0200

    Back to basics

--- GoProgramming-TheCompleteDeveloperGuide/slices/slices.go
+++ GoProgramming-TheCompleteDeveloperGuide/slices/slices.go
@@ -1,7 +1,12 @@
 package main
 
+import "fmt"
+
 type Part string
 
 func showLine(line []Part) {
-	part := line[i]
+	for i := 0; i <= len(line); i++ {
+		part := line[i]
+		fmt.Println(part)
+	}
 }
