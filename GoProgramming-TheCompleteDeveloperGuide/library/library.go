c2784b189dc38e6e61ce4ffeaf57a6f6cf55c65c
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-16 19:00:53 +0200

    Back to basics

--- GoProgramming-TheCompleteDeveloperGuide/library/library.go
+++ GoProgramming-TheCompleteDeveloperGuide/library/library.go
@@ -23,114 +23,117 @@ type BookEntry struct {
 	lended int
 }
 
-type Library struct {
-	members map[Name]Member
-	books   map[Title]BookEntry
-}
+// type Library struct {
+// 	members map[Name]Member
+// 	books   map[Title]BookEntry
+// }
 
-func printMemberAudit(member *Member) {
-	for title, audit := range member.books {
-		var returnTime string
-		if audit.checkIn.IsZero() {
-			returnTime = "[not returned yet]"
-		} else {
-			returnTime = audit.checkIn.String()
-		}
-		fmt.Println(member.name, ":", title, ":", audit.checkOut.String(), "throught", returnTime)
-	}
-}
+// func printMemberAudit(member *Member) {
+// 	for title, audit := range member.books {
+// 		var returnTime string
+// 		if audit.checkIn.IsZero() {
+// 			returnTime = "[not returned yet]"
+// 		} else {
+// 			returnTime = audit.checkIn.String()
+// 		}
+// 		fmt.Println(member.name, ":", title, ":", audit.checkOut.String(), "throught", returnTime)
+// 	}
+// }
 
-func printMemberAudits(library *Library) {
-	for _, member := range library.members {
-		printMemberAudit(&member)
-	}
-}
+// func printMemberAudits(library *Library) {
+// 	for _, member := range library.members {
+// 		printMemberAudit(&member)
+// 	}
+// }
 
-func printLibraryBooks(library *Library) {
-	fmt.Println()
-	for title, book := range library.books {
-		fmt.Println(title, "/ total:", book.total, "/ lended:", book.lended)
-	}
-	fmt.Println()
-}
+// func printLibraryBooks(library *Library) {
+// 	fmt.Println()
+// 	for title, book := range library.books {
+// 		fmt.Println(title, "/ total:", book.total, "/ lended:", book.lended)
+// 	}
+// 	fmt.Println()
+// }
 
-func checkoutBook(library *Library, title Title, member *Member) bool {
-	book, found := library.books[title]
-	if !found {
-		fmt.Println("Book is not part of the library")
-		return false
-	}
-	if book.lended == book.total {
-		fmt.Println("No more books available to lend")
-		return false
-	}
-	book.lended += 1
-	library.books[title] = book
-	member.books[title] = LendAudit{checkOut: time.Now()}
-	return true
-}
+// func checkoutBook(library *Library, title Title, member *Member) bool {
+// 	book, found := library.books[title]
+// 	if !found {
+// 		fmt.Println("Book is not part of the library")
+// 		return false
+// 	}
+// 	if book.lended == book.total {
+// 		fmt.Println("No more books available to lend")
+// 		return false
+// 	}
+// 	book.lended += 1
+// 	library.books[title] = book
+// 	member.books[title] = LendAudit{checkOut: time.Now()}
+// 	return true
+// }
 
-func returnBook(library *Library, title Title, member *Member) bool {
-	book, found := library.books[title]
-	if !found {
-		fmt.Println("Book is not part of the library")
-		return false
-	}
-	audit, found := member.books[title]
-	if !found {
-		fmt.Println("Member did not check out this book")
-		return false
-	}
-	book.lended -= 1
-	library.books[title] = book
+// func returnBook(library *Library, title Title, member *Member) bool {
+// 	book, found := library.books[title]
+// 	if !found {
+// 		fmt.Println("Book is not part of the library")
+// 		return false
+// 	}
+// 	audit, found := member.books[title]
+// 	if !found {
+// 		fmt.Println("Member did not check out this book")
+// 		return false
+// 	}
+// 	book.lended -= 1
+// 	library.books[title] = book
 
-	audit.checkIn = time.Now()
-	member.books[title] = audit
-	return true
-}
+// 	audit.checkIn = time.Now()
+// 	member.books[title] = audit
+// 	return true
+// }
 
 func main() {
-	library := Library{
-		books:   make(map[Title]BookEntry),
-		members: make(map[Name]Member),
-	}
+	myMap := make(map[Title]LendAudit)
+	myMap["Go Bootcamp"] = LendAudit{checkOut: time.Now(), checkIn: time.Now()}
+	fmt.Println(myMap)
+	// library := Library{
+	// 	books:   make(map[Title]BookEntry),
+	// 	members: make(map[Name]Member),
+	// }
 
-	library.books["Webapps in Go"] = BookEntry{
-		total:  4,
-		lended: 0,
-	}
-	library.books["The little Go Book"] = BookEntry{
-		total:  3,
-		lended: 0,
-	}
-	library.books["Let's learn Go"] = BookEntry{
-		total:  2,
-		lended: 0,
-	}
-	library.books["Go bootcamp"] = BookEntry{
-		total:  1,
-		lended: 0,
-	}
+	// library.books["Webapps in Go"] = BookEntry{
+	// 	total:  4,
+	// 	lended: 0,
+	// }
+	// library.books["The little Go Book"] = BookEntry{
+	// 	total:  3,
+	// 	lended: 0,
+	// }
+	// library.books["Let's learn Go"] = BookEntry{
+	// 	total:  2,
+	// 	lended: 0,
+	// }
+	// library.books["Go bootcamp"] = BookEntry{
+	// 	total:  1,
+	// 	lended: 0,
+	// }
 
-	library.members["Bob"] = Member{"Bob", make(map[Title]LendAudit)}
-	library.members["John"] = Member{"John", make(map[Title]LendAudit)}
-	library.members["Erik"] = Member{"Erik", make(map[Title]LendAudit)}
+	// library.members["Bob"] = Member{"Bob", make(map[Title]LendAudit)}
+	// library.members["John"] = Member{"John", make(map[Title]LendAudit)}
+	// library.members["Erik"] = Member{"Erik", make(map[Title]LendAudit)}
 
-	fmt.Println("\nInitial:")
-	printLibraryBooks(&library)
-	printMemberAudits(&library)
+	// fmt.Println("\nInitial:")
+	// printLibraryBooks(&library)
+	// printMemberAudits(&library)
 
-	member := library.members["Jayson"]
-	checkedOut := checkoutBook(&library, "Go Bootcamp", &member)
-	fmt.Println("\nCheck out a book:")
-	if checkedOut {
-		printLibraryBooks(&library)
-		printMemberAudits(&library)
-	}
-	returned := returnBook(&library, "Go Bootcamp", &member)
-	fmt.Println("\nCheck ia a book:")
-	if returned {
-		printLibraryBooks(&library)
-		printMemberAudits(&library)
-	}
+	// member := library.members["Jayson"]
+	// checkedOut := checkoutBook(&library, "Go Bootcamp", &member)
+	// fmt.Println("\nCheck out a book:")
+	// if checkedOut {
+	// 	printLibraryBooks(&library)
+	// 	printMemberAudits(&library)
+	// }
+	// returned := returnBook(&library, "Go Bootcamp", &member)
+	// fmt.Println("\nCheck ia a book:")
+	// if returned {
+	// 	printLibraryBooks(&library)
+	// 	printMemberAudits(&library)
+	// }
 }
