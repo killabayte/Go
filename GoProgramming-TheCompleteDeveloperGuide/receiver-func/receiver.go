dd1fa0ee7295b342ca1f9f7617fff4ccbf82f60a
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-17 18:15:47 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/receiver-func/receiver.go
+++ GoProgramming-TheCompleteDeveloperGuide/receiver-func/receiver.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Space struct {
 	occupied bool
 }
@@ -10,4 +12,20 @@ type ParkingLot struct {
 
 func occupySpace(lot *ParkingLot, spaceNum int) {
 	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingLot) occupySpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingLot) vacateSpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = false
+}
+
+func main() {
+	lot := ParkingLot{spaces: make([]Space, 10)}
+	fmt.Println(lot)
+	lot.occupySpace(1)
+	occupySpace(&lot, 2)
+	fmt.Println(lot)
 }
