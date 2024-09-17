2853a517129ed9b86dadc2f7f97c13711c8c5986
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-17 09:36:32 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/library/library.go
+++ GoProgramming-TheCompleteDeveloperGuide/library/library.go
@@ -23,52 +23,52 @@ type BookEntry struct {
 	lended int
 }
 
-// type Library struct {
-// 	members map[Name]Member
-// 	books   map[Title]BookEntry
-// }
+type Library struct {
+	members map[Name]Member
+	books   map[Title]BookEntry
+}
 
-// func printMemberAudit(member *Member) {
-// 	for title, audit := range member.books {
-// 		var returnTime string
-// 		if audit.checkIn.IsZero() {
-// 			returnTime = "[not returned yet]"
-// 		} else {
-// 			returnTime = audit.checkIn.String()
-// 		}
-// 		fmt.Println(member.name, ":", title, ":", audit.checkOut.String(), "throught", returnTime)
-// 	}
-// }
+func printMemberAudit(member *Member) {
+	for title, audit := range member.books {
+		var returnTime string
+		if audit.checkIn.IsZero() {
+			returnTime = "[not returned yet]"
+		} else {
+			returnTime = audit.checkIn.String()
+		}
+		fmt.Println(member.name, ":", title, ":", audit.checkOut.String(), "throught", returnTime)
+	}
+}
 
-// func printMemberAudits(library *Library) {
-// 	for _, member := range library.members {
-// 		printMemberAudit(&member)
-// 	}
-// }
+func printMemberAudits(library *Library) {
+	for _, member := range library.members {
+		printMemberAudit(&member)
+	}
+}
 
-// func printLibraryBooks(library *Library) {
-// 	fmt.Println()
-// 	for title, book := range library.books {
-// 		fmt.Println(title, "/ total:", book.total, "/ lended:", book.lended)
-// 	}
-// 	fmt.Println()
-// }
+func printLibraryBooks(library *Library) {
+	fmt.Println()
+	for title, book := range library.books {
+		fmt.Println(title, "/ total:", book.total, "/ lended:", book.lended)
+	}
+	fmt.Println()
+}
 
-// func checkoutBook(library *Library, title Title, member *Member) bool {
-// 	book, found := library.books[title]
-// 	if !found {
-// 		fmt.Println("Book is not part of the library")
-// 		return false
-// 	}
-// 	if book.lended == book.total {
-// 		fmt.Println("No more books available to lend")
-// 		return false
-// 	}
-// 	book.lended += 1
-// 	library.books[title] = book
-// 	member.books[title] = LendAudit{checkOut: time.Now()}
-// 	return true
-// }
+func checkoutBook(library *Library, title Title, member *Member) bool {
+	book, found := library.books[title]
+	if !found {
+		fmt.Println("Book is not part of the library")
+		return false
+	}
+	if book.lended == book.total {
+		fmt.Println("No more books available to lend")
+		return false
+	}
+	book.lended += 1
+	library.books[title] = book
+	member.books[title] = LendAudit{checkOut: time.Now()}
+	return true
+}
 
 // func returnBook(library *Library, title Title, member *Member) bool {
 // 	book, found := library.books[title]
