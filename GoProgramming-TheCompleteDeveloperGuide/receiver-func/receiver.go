427bd4045d80d33864fb8d6a4299a6a175336331
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-18 12:01:20 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/receiver-func/receiver.go
+++ GoProgramming-TheCompleteDeveloperGuide/receiver-func/receiver.go
@@ -3,7 +3,21 @@ package main
 type Player struct {
 	name      string
 	MaxHealth int
+	MaxEnergy int
 	Health    int
 	Energy    int
-	MaxEnergy int
+}
+
+func NewPlayer() Player {
+	return Player{
+		MaxHealth: 5,
+	}
+}
+
+func (p *Player) modifyHealth(modifyValueLow int, modifyValueTop int) {
+	if modifyValueLow != 0 {
+		p.MaxHealth = p.MaxHealth - modifyValueLow
+	} else if modifyValueTop != 0 {
+		p.MaxHealth = p.MaxHealth + modifyValueTop
+	}
 }
