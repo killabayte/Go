76f6170c2812cf7ae16478aec3d14df34f54d88d
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-19 20:49:07 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/iota/iota.go
+++ GoProgramming-TheCompleteDeveloperGuide/iota/iota.go
@@ -1,8 +1,25 @@
 package main
 
+type Calculator byte
+
 const (
-	Add byte = iota
+	Add Calculator = iota
 	Subtract
 	Multiply
 	Divide
 )
+
+func (c Calculator) Calculate(n1, n2 int) int {
+	switch c {
+	case Add:
+		return (n1 + n2)
+	case Subtract:
+		return (n1 - n2)
+	case Multiply:
+		return (n1 * n2)
+	case Divide:
+		return (n1 / n2)
+	default:
+		return 0
+	}
+}
