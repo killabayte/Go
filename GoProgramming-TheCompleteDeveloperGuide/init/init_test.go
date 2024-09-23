d6c728e5f011d4b3193d1e1e41c283ceee338b16
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-23 13:24:59 +0200

    Back to basics

--- GoProgramming-TheCompleteDeveloperGuide/init/init_test.go
+++ GoProgramming-TheCompleteDeveloperGuide/init/init_test.go
@@ -5,6 +5,22 @@ import "testing"
 func TestIsValidEmail(t *testing.T) {
 	data := "email@example.com"
 	if !isValidEmail(data) {
-		t.Error("IsValidEmail(%v)=false, want true", data)
+		t.Errorf("IsValidEmail(%v)=false, want true", data)
+	}
+}
+
+func TestIsValidEmailTable(t *testing.T) {
+	table := []struct {
+		email string
+		want  bool
+	}{
+		{"email@example.com", true},
+		{"missing@tld", false},
+	}
+	for _, data := range table {
+		result := isValidEmail(data.email)
+		if result != data.want {
+			t.Errorf("%v: %t, want: %t", data.email, result, data.want)
+		}
 	}
 }
