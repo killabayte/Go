2c6e606b918279a5aa94f065b42e8ada6c19d374
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-23 19:19:41 +0200

    Back to basics

---
+++ GoProgramming-TheCompleteDeveloperGuide/testing/queue_test.go
@@ -0,0 +1,15 @@
+package queue
+
+import "testing"
+
+func TestAddQueue(t *testing.T) {
+	q := New(3)
+	for i := 0; i < 3; i++ {
+		if len(q.items) != i {
+			t.Errorf("Incorrect queue element count: %v, want %v", len(q.items), i)
+		}
+		if !q.Append(i) {
+			t.Errorf("Failed to append item %v to queue", i)
+		}
+	}
+}
