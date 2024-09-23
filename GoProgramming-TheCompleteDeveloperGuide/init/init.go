5af4879d2285a9abc801a8ed2878a9d1f6978d86
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-23 13:06:12 +0200

    Back to basics

--- GoProgramming-TheCompleteDeveloperGuide/init/init.go
+++ GoProgramming-TheCompleteDeveloperGuide/init/init.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var EmailExpr *regexp.Regexp
+
+func init() {
+	compiled, ok := regexp.Compile(`.+@.+\..+`)
+	if ok != nil {
+		panic("Failed to complile regular expression")
+	}
+	EmailExpr = compiled
+	fmt.Println("Regular expresssion compiled successfully")
+}
+
+func isValidEmail(addr string) bool {
+	return EmailExpr.Match([]byte(addr))
+}
+
+func main() {
+	fmt.Println(isValidEmail("invalid"))
+	fmt.Println(isValidEmail("valid@example.com"))
+	fmt.Println(isValidEmail("invalid@example"))
+}
