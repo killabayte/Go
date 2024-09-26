476d4e6b1f396e7783b24ee2fc6e289dc0342a36
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-26 10:59:18 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/receiver-func/receiver_test.go
+++ GoProgramming-TheCompleteDeveloperGuide/receiver-func/receiver_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMaxResources(t *testing.T) {
 	np := NewPlayer()
@@ -16,5 +18,19 @@ func TestMaxResources(t *testing.T) {
 	}
 	if np.MaxEnergy > defaultMaxEnergy {
 		t.Errorf("Max energy: %v is bigger than maximum %v", np.MaxEnergy, defaultMaxEnergy)
+	}
+}
+
+func TestMinResources(t *testing.T) {
+	np := NewPlayer()
+
+	np.modifyHealth(np.MaxHealth+1, 0)
+	np.modifyEnergy(np.MaxEnergy+1, 0)
+
+	if np.Health < 0 {
+		t.Errorf("Players health: %v is lower than zero", np.Health)
+	}
+	if np.Energy < 0 {
+		t.Errorf("Players energy: %v is lower than zero", np.Energy)
 	}
 }
