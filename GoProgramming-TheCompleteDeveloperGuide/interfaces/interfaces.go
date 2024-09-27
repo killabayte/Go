03009cf6c161e1fe5ba19dac592171971bccc568
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-27 15:47:31 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/interfaces/interfaces.go
+++ GoProgramming-TheCompleteDeveloperGuide/interfaces/interfaces.go
@@ -1,33 +1,5 @@
 package main
 
-import "fmt"
-
-type Preparer interface {
-	PrepareDish()
-}
-
-type Chicken string
-type Salad string
-
-func (c Chicken) PrepareDish() {
-	fmt.Println("Cook chicken")
-}
-func (s Salad) PrepareDish() {
-	fmt.Println("Chop salad")
-	fmt.Println("Add dressing")
-}
-
-func prepareDishes(dishes []Preparer) {
-	fmt.Println("Prepare dishes:")
-	for i := 0; i < len(dishes); i++ {
-		dish := dishes[i]
-		fmt.Printf("--Dish: %v--\n", dish)
-		dish.PrepareDish()
-	}
-	fmt.Println()
-}
-
 func main() {
-	dishes := []Preparer{Chicken("Chicken Wings"), Salad("Iceberg salad")}
-	prepareDishes(dishes)
+
 }
