93d5653b213c632ddd60b573810ba5b60026e78e
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-27 15:01:28 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/interfaces/interfaces.go
+++ GoProgramming-TheCompleteDeveloperGuide/interfaces/interfaces.go
@@ -12,6 +12,11 @@ type Salad string
 func (c Chicken) PrepareDish() {
 	fmt.Println("Cook chicken")
 }
+func (s Salad) PrepareDish() {
+	fmt.Println("Chop salad")
+	fmt.Println("Add dressing")
+
+}
 
 func main() {
 
