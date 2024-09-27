47fbe1cc032c4e3873a0ad1b4be7a0f8386c81ac
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-27 14:50:02 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/interfaces/interfaces.go
+++ GoProgramming-TheCompleteDeveloperGuide/interfaces/interfaces.go
@@ -1,11 +1,17 @@
 package main
 
+import "fmt"
+
 type Preparer interface {
 	PrepareDish()
 }
 
 type Chicken string
 type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("Cook chicken")
+}
 
 func main() {
 
