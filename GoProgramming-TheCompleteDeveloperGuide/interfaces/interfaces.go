0b058d00f05751d67b4e08ce45ced4f513a7cbee
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-27 16:00:05 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/interfaces/interfaces.go
+++ GoProgramming-TheCompleteDeveloperGuide/interfaces/interfaces.go
@@ -40,11 +40,11 @@ func (t Truck) PickLift() Lift {
 func sendToLift(p LiftPicker) {
 	switch p.PickLift() {
 	case SmallLift:
-		fmt.Println("Send %v to small lift\n", p)
+		fmt.Printf("Send %v to small lift\n", p)
 	case StandardLift:
-		fmt.Println("Send %v to standard lift\n", p)
+		fmt.Printf("Send %v to standard lift\n", p)
 	case LargeLift:
-		fmt.Println("Send %v to large lift\n", p)
+		fmt.Printf("Send %v to large lift\n", p)
 	}
 }
 
