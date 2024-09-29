bb27553fa5663fe5afc7a0472d0049134d5de363
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-29 20:40:57 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/errors/errors.go
+++ GoProgramming-TheCompleteDeveloperGuide/errors/errors.go
@@ -1,5 +1,7 @@
 package timeparse
 
+import "fmt"
+
 type Time struct {
 	hour, minute, second int
 }
@@ -7,4 +9,8 @@ type Time struct {
 type TimeParseError struct {
 	msg   string
 	input string
+}
+
+func (t *TimeParseError) Error() string {
+	return fmt.Sprintf("%v: %v", t.msg, t.input)
 }
