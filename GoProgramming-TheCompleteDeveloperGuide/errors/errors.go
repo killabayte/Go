8c1cd24f36214453110e7546ab77886a73cd1ae2
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-29 20:38:36 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/errors/errors.go
+++ GoProgramming-TheCompleteDeveloperGuide/errors/errors.go
@@ -1,28 +1,5 @@
-package main
+package timeparse
 
-import (
-	"errors"
-	"fmt"
-)
-
-type Stuff struct {
-	values []int
-}
-
-func (s *Stuff) Get(index int) (int, error) {
-	if index > len(s.values) {
-		return 0, errors.New(fmt.Sprintf("No element at index %v", index))
-	} else {
-		return s.values[index], nil
-	}
-}
-
-func main() {
-	stuff := Stuff{}
-	value, err := stuff.Get(1)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Value is", value)
-	}
+type Time struct {
+	hour, minute, second int
 }
