87747854ac4c7dd6a2942a5c5411cc4e1fba862b
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-29 10:56:17 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/errors/errors.go
+++ GoProgramming-TheCompleteDeveloperGuide/errors/errors.go
@@ -1,11 +1,20 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Stuff struct {
 	values []int
 }
 
-func (s *Stuff) Get(index int) (int, error){
-	if index > len(s.values)
+func (s *Stuff) Get(index int) (int, error) {
+	if index > len(s.values) {
+		return 0, errors.New(fmt.Sprintf("No element at index %v", index))
+	} else {
+		return s.values[index], nil
+	}
 }
 
 func main() {
