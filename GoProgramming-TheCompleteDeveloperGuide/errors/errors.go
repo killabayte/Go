f8f8c201d7d80414c591483fbcf39c52ac5b7720
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-09-30 09:40:00 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/errors/errors.go
+++ GoProgramming-TheCompleteDeveloperGuide/errors/errors.go
@@ -28,7 +28,7 @@ func ParseTime(input string) (Time, error) {
 		if err != nil {
 			return Time{}, &TimeParseError{fmt.Sprintf("Error parsing hour: %v", err), input}
 		}
-		minute, err := strconv.Atoi(components[0])
+		minute, err := strconv.Atoi(components[1])
 		if err != nil {
 			return Time{}, &TimeParseError{fmt.Sprintf("Error parsing minute: %v", err), input}
 		}
