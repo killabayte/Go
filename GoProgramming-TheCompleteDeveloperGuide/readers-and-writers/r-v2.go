345329f84a70b364dbee4e4899880fa7b3ae6e39
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-10-02 13:58:00 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/readers-and-writers/r-v2.go
+++ GoProgramming-TheCompleteDeveloperGuide/readers-and-writers/r-v2.go
@@ -7,20 +7,15 @@ import (
 )
 
 func main_old_v2() {
-
 	scanner := bufio.NewScanner(os.Stdin)
 	lines := make([]string, 0, 5)
-
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
-
 	if scanner.Err() != nil {
 		fmt.Println(scanner.Err())
 	}
-
 	fmt.Printf("Line count: %v\n", len(lines))
-
 	for _, line := range lines {
 		fmt.Printf("Line: %v\n", line)
 	}
