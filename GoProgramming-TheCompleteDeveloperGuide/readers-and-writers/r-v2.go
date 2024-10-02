cca3ec10cb19c3dd6036b7666de5c76278f5d53e
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-10-02 13:57:45 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/readers-and-writers/r-v2.go
+++ GoProgramming-TheCompleteDeveloperGuide/readers-and-writers/r-v2.go
@@ -18,7 +18,9 @@ func main_old_v2() {
 	if scanner.Err() != nil {
 		fmt.Println(scanner.Err())
 	}
+
 	fmt.Printf("Line count: %v\n", len(lines))
+
 	for _, line := range lines {
 		fmt.Printf("Line: %v\n", line)
 	}
