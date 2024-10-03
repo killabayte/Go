ac8391cafbc0054955c8a38851d2b31505b524e0
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-10-03 10:07:12 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/readers-and-writers/r-test.go
+++ GoProgramming-TheCompleteDeveloperGuide/readers-and-writers/r-test.go
@@ -8,10 +8,10 @@ import (
 	"strings"
 )
 
-command_count := 0
-non_blanck_count := 0
-
 func main() {
+	command_count := 0
+	non_blanck_count := 0
+
 	r := bufio.NewReader(os.Stdin)
 
 	for {
@@ -20,16 +20,18 @@ func main() {
 		if inputErr == io.EOF {
 			break
 		}
-		if len(l) > 0{
-			non_blanck_count ++
+		if len(l) > 0 {
+			non_blanck_count++
 		}
 		switch l {
 		case l == "hello":
-			command_count ++
+			command_count++
 			fmt.Println("Hi User, nice to meet you!")
 		case l == "bye":
-			command_count ++
+			command_count++
 			fmt.Println("Have a good day!And come back soon")
 		}
 	}
+	fmt.Println("The number of commands entered:", command_count)
+	fmt.Println("The number of non-blank lines:", non_blanck_count)
 }
