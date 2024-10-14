9513bdb642856efa24114eed1f9141e2a4520843
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-10-14 12:36:15 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/embedding/embedding.go
+++ GoProgramming-TheCompleteDeveloperGuide/embedding/embedding.go
@@ -16,8 +16,6 @@ type MemoryUsage struct {
 func main() {
 
 	bandwidth := BandwidthUsage{[]Bytes{50000, 100000, 130000, 80000, 90000}}
-
 	temp := CpuTemp{[]Celcius{50, 51, 53, 51, 52}}
-
 	memory := MemoryUsage{[]Bytes{800000, 800000, 810000, 820000, 800000}}
 }
