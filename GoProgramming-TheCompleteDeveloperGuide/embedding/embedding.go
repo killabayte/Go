c699900b4e7e2fa712a60b9bc35082b8ccea4df4
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-10-16 16:43:52 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/embedding/embedding.go
+++ GoProgramming-TheCompleteDeveloperGuide/embedding/embedding.go
@@ -57,6 +57,5 @@ func main() {
 	fmt.Printf("Average bandwidth usage: %v\n", dash.AverageBandwidth())
 
 	fmt.Printf("Average temp: %v\n", dash.AverageCpuTemp())
-
 	fmt.Printf("Avarage memory usage: %v\n", dash.AverageMemUsage())
 }
