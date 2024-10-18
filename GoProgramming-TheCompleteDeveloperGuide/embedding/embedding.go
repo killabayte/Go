eee1f756120570867a3a0b62adadbb91bfa0efc5
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-10-18 11:57:43 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/embedding/embedding.go
+++ GoProgramming-TheCompleteDeveloperGuide/embedding/embedding.go
@@ -47,6 +47,7 @@ func main() {
 	bandwidth := BandwidthUsage{[]Bytes{50000, 100000, 130000, 80000, 90000}}
 	temp := CpuTemp{[]Celcius{50, 51, 53, 51, 52}}
 	memory := MemoryUsage{[]Bytes{800000, 800000, 810000, 820000, 800000}}
+
 	dash := Dashboard{
 		BandwidthUsage: bandwidth,
 		CpuTemp:        temp,
