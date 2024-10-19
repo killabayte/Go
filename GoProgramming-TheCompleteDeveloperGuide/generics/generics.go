7a546ca155491def9c4c9872e268aba771eae31a
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-10-19 12:07:16 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/generics/generics.go
+++ GoProgramming-TheCompleteDeveloperGuide/generics/generics.go
@@ -6,6 +6,15 @@ const{
 	High
 }
 
+type PriorityQueue[P comparable, V any] struct{
+	items map[P][]V
+	priorities []P
+}
+
+func NewPriorityQueue[P comparable, V any](priorities []P) PriorityQueue[P, V]{
+	return PriorityQueue[P, V]{items: make(map[P][]V), priorities: priorities}
+}
+
 func main(){
-	
+
 }
