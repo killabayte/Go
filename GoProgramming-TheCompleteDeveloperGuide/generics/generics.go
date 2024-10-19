ea1b001e2c8fb3f29e419654ff3c3c687fe0e221
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-10-19 12:18:15 +0200

    Back to basics: Go

--- GoProgramming-TheCompleteDeveloperGuide/generics/generics.go
+++ GoProgramming-TheCompleteDeveloperGuide/generics/generics.go
@@ -15,6 +15,18 @@ func (pq *PriorityQueue[P, V]) Add(priority P, value V) {
 	pq.items[priority] = append(pq.items[priority], value)
 }
 
+func (pq *PriorityQueue[P, V]) Next() (V, bool) {
+	for i := 0; i < len(pq.priorities); i++ {
+		priority := pq.priorities[i]
+		items := pq.items[priority]
+		if len(items) > 0 {
+			next := items[0]
+			pq.items[priority] = items[1:]
+			return next, true
+		}
+	}
+}
+
 func NewPriorityQueue[P comparable, V any](priorities []P) PriorityQueue[P, V] {
 	return PriorityQueue[P, V]{items: make(map[P][]V), priorities: priorities}
 }
